01c7fed923b123fd595c16da0daed95c30cf1e4e
resapopo <64885426+resapopo@users.noreply.github.com>
2020-06-28 08:37:42 +0900

    for test

--- js/main-audio.js
+++ js/main-audio.js
@@ -40,6 +40,7 @@ const formInput = document.querySelector('input#index');
 // レコードボタン
 recordButton.addEventListener('click', () => {
   if (recordButton.textContent === 'Start Recording') {
+    recordButton.textContent = 'Clicked!';
     startRecording();
   } else {
     stopRecording();
@@ -48,6 +49,50 @@ recordButton.addEventListener('click', () => {
   }
 });
 //recordButton
+
+function startRecording() {
+  // 初期化
+  recordedBlobs = [];
+  // コーデックを推奨順に指定
+  let options = {mimeType: 'audio/ogg; codecs=opus'};
+  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+    console.error(`${options.mimeType} is not supported`);
+    options = {mimeType: 'audio/ogg; codecs=vorbis'};
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+      console.error(`${options.mimeType} is not supported`);
+      options = {mimeType: 'audio/mpeg'};
+      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.error(`${options.mimeType} is not supported`);
+        options = {mimeType: ''};
+      }
+    }
+  }
+
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  }
+
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  recordButton.textContent = 'Stop Recording';
+  playButton.disabled = true;
+  downloadButton.disabled = true;
+  mediaRecorder.onstop = (event) => {
+    console.log('Recorder stopped: ', event);
+    console.log('Recorded Blobs: ', recordedBlobs);
+  };
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log('MediaRecorder started', mediaRecorder);
+}
+// startRecording
+
+function stopRecording() {
+  mediaRecorder.stop();
+}
 
 // プレイ（プッシュ）ボタン
 const playButton = document.querySelector('button#play');
@@ -238,50 +283,6 @@ function handleDataAvailable(event) {
   if (event.data && event.data.size > 0) {
     recordedBlobs.push(event.data);
   }
-}
-
-function startRecording() {
-  // 初期化
-  recordedBlobs = [];
-  // コーデックを推奨順に指定
-  let options = {mimeType: 'audio/ogg; codecs=opus'};
-  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-    console.error(`${options.mimeType} is not supported`);
-    options = {mimeType: 'audio/ogg; codecs=vorbis'};
-    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-      console.error(`${options.mimeType} is not supported`);
-      options = {mimeType: 'audio/mpeg'};
-      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-        console.error(`${options.mimeType} is not supported`);
-        options = {mimeType: ''};
-      }
-    }
-  }
-
-  try {
-    mediaRecorder = new MediaRecorder(window.stream, options);
-  } catch (e) {
-    console.error('Exception while creating MediaRecorder:', e);
-    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
-    return;
-  }
-
-  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-  recordButton.textContent = 'Stop Recording';
-  playButton.disabled = true;
-  downloadButton.disabled = true;
-  mediaRecorder.onstop = (event) => {
-    console.log('Recorder stopped: ', event);
-    console.log('Recorded Blobs: ', recordedBlobs);
-  };
-  mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.start();
-  console.log('MediaRecorder started', mediaRecorder);
-}
-// startRecording
-
-function stopRecording() {
-  mediaRecorder.stop();
 }
 
 // access yours mic
