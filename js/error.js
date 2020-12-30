0ed7aa3183cde7bbeefd30bc2ec0b6227c097479
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2020-12-30 09:55:54 +0900

    エラーメッセージサンプル

---
+++ js/error.js
@@ -0,0 +1,5 @@
+﻿function SendErrorMsg (pgName, methodName,errorMessage) {
+	var ut = navigator.userAgent;
+	var postData = {"日本語":pgName,"methodName":methodName,"errorMessage":errorMessage,"userAgent":ut};
+	$.post("https://prod-12.japaneast.logic.azure.com:443/workflows/4a1b48dd6d9d45e7a4997c21abeacdc7/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=64EMXa3MMqVk3YAhWG5ghzsHe-Xna6YJvW0l99xbfOk", postData);
+}
